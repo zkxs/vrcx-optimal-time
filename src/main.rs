03bcdb377983e39445436cc80f53674e955d93e3
Michael Ripley <zkxs00@gmail.com>
2022-10-29 05:31:17 -0500

    Minor code cleanup

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ use chrono::naive::NaiveTime;
 use chrono::{Datelike, DateTime, Duration, DurationRound, Local, Timelike, Utc, Weekday};
 use num_traits::cast::FromPrimitive;
 use rusqlite::{Connection, OpenFlags};
-use crate::EventType::{Offline, Online};
 
 mod config;
 
@@ -19,6 +18,7 @@ const MINUTES_PER_HOUR: u32 = 60;
 const SECONDS_PER_MINUTE: u32 = 60;
 const MINUTES_PER_DAY: u32 = HOURS_PER_DAY * MINUTES_PER_HOUR;
 
+// indices of the columns we get back in our sqlite query result set
 const COLUMN_INDEX_CREATED_AT: usize = 0;
 const COLUMN_INDEX_USER_ID: usize = 1;
 const COLUMN_INDEX_DISPLAY_NAME: usize = 2;
@@ -44,28 +44,28 @@ fn main() {
             | OpenFlags::SQLITE_OPEN_URI
             | OpenFlags::SQLITE_OPEN_NO_MUTEX).unwrap();
 
-    // set up data structures we'll need for the procedure
-    let mut user_online_time: HashMap<String, DateTime<Utc>> = HashMap::new();
-    let mut buckets: Vec<Vec<u32>> = build_buckets(buckets_per_day);
-
-    // run the statement
+    // build and run the query
     let stripped_user_id = config.your_user_id.replace('-', "").replace('_', "");
     let table_name = format!("{}{}", stripped_user_id, TABLE_SUFFIX);
     let statement = format!("select created_at, user_id, display_name, type from {} order by id", table_name);
     let mut statement = db.prepare(&statement).unwrap();
     let user_online_offline_events = statement.query_map((), |row| Row::try_from(row)).unwrap();
 
+    // set up data structures we'll need for the procedure
+    let mut user_online_time: HashMap<String, DateTime<Utc>> = HashMap::new();
+    let mut buckets: Vec<Vec<u32>> = build_buckets(buckets_per_day);
+
     // process the results
     for row in user_online_offline_events {
         let row = row.unwrap();
         if is_user_allowed(&row.user_id, &config.friend_ids) {
             match row.event_type {
-                Online => {
+                EventType::Online => {
                     // it is intentional that this overwrites previous Online events,
                     // because given two Online events in a row we should drop the first one
                     user_online_time.insert(row.user_id, row.created_at);
                 }
-                Offline => {
+                EventType::Offline => {
                     let online_time = user_online_time.remove(&row.user_id);
                     if let Some(online_time) = online_time {
                         let range = row.created_at.signed_duration_since(online_time);
@@ -95,6 +95,7 @@ fn build_buckets(buckets_per_day: usize) -> Vec<Vec<u32>> {
     vec![vec![0; buckets_per_day]; DAYS_PER_WEEK]
 }
 
+/// update buckets that a provided range encompasses
 fn update_buckets(bucket_duration: Duration, bucket_duration_minutes: u32, start_time: DateTime<Utc>, end_time: DateTime<Utc>, buckets: &mut [Vec<u32>]) {
     let end_time = end_time.with_timezone(&Local);
     let mut start_time = start_time.with_timezone(&Local);
@@ -112,6 +113,7 @@ fn update_buckets(bucket_duration: Duration, bucket_duration_minutes: u32, start
     }
 }
 
+/// print bucket data to console
 fn print_buckets(bucket_duration_seconds: u32, buckets_per_day: usize, buckets: Vec<Vec<u32>>) {
     // header
     print!("bucket");
@@ -132,16 +134,19 @@ fn print_buckets(bucket_duration_seconds: u32, buckets_per_day: usize, buckets: 
     }
 }
 
+/// convert a bucket index into a label string
 fn bucket_index_to_label(bucket_duration_seconds: u32, bucket_index: usize) -> String {
     let time = bucket_index_to_time(bucket_duration_seconds, bucket_index);
     format!("{:02}:{:02}", time.hour(), time.minute())
 }
 
+/// convert a bucket index to the time of day
 fn bucket_index_to_time(bucket_duration_seconds: u32, bucket_index: usize) -> NaiveTime {
     let seconds_from_midnight = bucket_duration_seconds * u32::try_from(bucket_index).unwrap();
     NaiveTime::from_num_seconds_from_midnight(seconds_from_midnight, 0)
 }
 
+/// check if a given user has been filtered out by our configuration
 fn is_user_allowed(user_id: &str, friend_ids: &Option<HashSet<String>>) -> bool {
     if let Some(friend_ids) = friend_ids {
         friend_ids.contains(user_id)
@@ -151,6 +156,7 @@ fn is_user_allowed(user_id: &str, friend_ids: &Option<HashSet<String>>) -> bool 
     }
 }
 
+/// represents a row from the friend online/offline table
 struct Row {
     created_at: DateTime<Utc>,
     user_id: String,
@@ -181,6 +187,7 @@ impl TryFrom<&rusqlite::Row<'_>> for Row {
     }
 }
 
+/// the type of an online/offline event
 enum EventType {
     Online,
     Offline,
@@ -191,8 +198,8 @@ impl TryFrom<&str> for EventType {
 
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         match value {
-            "Online" => Ok(Online),
-            "Offline" => Ok(Offline),
+            "Online" => Ok(EventType::Online),
+            "Offline" => Ok(EventType::Offline),
             _ => Err(rusqlite::Error::InvalidColumnType(COLUMN_INDEX_EVENT_TYPE, value.to_string(), rusqlite::types::Type::Text))
         }
     }
