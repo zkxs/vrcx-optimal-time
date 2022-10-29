9a33a29739ecc775840cb437d2b17e929d7eeb2d
Michael Ripley <zkxs00@gmail.com>
2022-10-28 21:42:09 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# VRCX Optimal Time
+
+Uses your stored [VRCX](https://github.com/pypy-vrc/VRCX) data to chart the optimal times for you to play VRChat to be online at the same time as your friends. 
+
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+// Copyright 2022 Michael Ripley
+// This file is part of vrcx-optimal-time.
+// vrcx-optimal-time is licensed under the MIT license (see LICENSE file for details).
+
+fn main() {
+    println!("Hello, world!");
+}
